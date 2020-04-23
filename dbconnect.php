75b404e5f113924d8980d98cacea55881ca04d8b
0-justonce-0 <64194289+0-justonce-0@users.noreply.github.com>
2020-04-24 01:42:41 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,13 +1,16 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "theater";
+$servername = "ec2-3-231-46-238.compute-1.amazonaws.com";
+$username = "tiqxtmdxejqirl";
+$password = "5a13415c33a740c9e0a29d0874cb47934033c4418872cd3aecfeeec3d6b872d6";
+$port = "5432";
+$dbname = "dd18of86m8s8od";
 
-$conn = mysqli_connect($servername,$username,$password,$dbname);
+$connection = "host=$servername, port=$port, user=$username, password=$password, dbname=$dbname";
+//$conn = pg_connect($connection);
+$conn = new PDO($connection);
 
-if(mysqli_connect_errno()) {
-	echo 'Failed to connect to MySQL ' . mysqli_connect_error();
-}
+// if(mysqli_connect_errno()) {
+// 	echo 'Failed to connect to MySQL ' . mysqli_connect_error();
+// }
 ?>
