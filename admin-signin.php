41444516f3ccd9e0835404561ca306ef9c874848
0-justonce-0 <64194289+0-justonce-0@users.noreply.github.com>
2020-04-24 22:41:14 +0700

    Update admin-signin.php

--- admin-signin.php
+++ admin-signin.php
@@ -20,71 +20,86 @@ if($row['password']==$pwd) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PPDJ </title>
+    <title>PPDJ - Admin Sign in</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link rel="shortcut icon" type="image/x-icon" href="https://lh3.googleusercontent.com/-HtZivmahJYI/VUZKoVuFx3I/AAAAAAAAAcM/thmMtUUPjbA/Blue_square_A-3.PNG" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="css/forcompany.css">
     <link rel="stylesheet" href="css/admin-signin.css">
-    <link rel="stylesheet" href="css/customer-signin.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="shortcut icon" type="image/x-icon" href="https://lh3.googleusercontent.com/-HtZivmahJYI/VUZKoVuFx3I/AAAAAAAAAcM/thmMtUUPjbA/Blue_square_A-3.PNG" />
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script src="js/login.js"> </script>
 </head>
-<body>
+<body class="shape">
     <nav class="navbar navbar-default navbar-fixed-top">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-                <a class="navbar-brand" href="index.html">Home</a>
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html"> Home</a>
                 <a class="navbar-brand" href="showtime.html">Showtime</a>
-			</div>
-			<div class="collapse navbar-collapse" id="myNavbar">
-				<ul class="nav navbar-nav navbar-right">
-					<li class="dropdown" id = "new">
-						<a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-user"> Sign in&nbsp;</span><span class="caret"></span>
-						</a>
-						<ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">
-						  <li><a href="register.html">Register</a></li>
-						  
-						  <li class="dropdown-submenu">
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+
+                    <li class="dropdown" id = "new">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-user"> Sign in&nbsp;</span><span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">
+                          <li><a href="register.html">Register</a></li>
+                          
+                          <li class="dropdown-submenu">
                             <a tabindex="-1" href="#">Sign in</a>
-							<ul class="dropdown-menu">
-							  <li><a tabindex="-1" href="#">Manager Sign in</a></li>
-							  <li><a href="customer-signin.html">Customer Sign in</a></li>
-							  </li>
-						
-					
-							</ul>
-						  </li>
-						
-						</ul>
-					</li>
-					  <li class="dropdown" id = "old">
-						<a class="dropdown-toggle" data-toggle="dropdown" href="#" ><span class="glyphicon glyphicon-user" id="wuser">Welcom!</span>
-						<span class="caret"></span>
-						</a>
-						<ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">
-							<li><a href="showhistory.php">History</a></li>							
-							<li><a href="#" id="logout">Sign out</a></li>
-						</ul>
-						</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
-	<div class="jumbotron text-center">
-		<h1>PPDJ <span class="sigma">Σ</span>-nema</h1> 
-	</div>
-	
-	<div class="container" id="homepage">
+                            <ul class="dropdown-menu">
+                              <li><a tabindex="-1" href="admin-signin.html">Admin Sign in</a></li>
+                              <li><a href="customer-signin.html">Customer Sign in</a></li>
+                              </li>
+                        
+                    
+                            </ul>
+                          </li>
+                        
+                        </ul>
+                    </li>
+                      <li class="dropdown" id = "old">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#" ><span class="glyphicon glyphicon-user" id="wuser">Welcome!</span>
+                        <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">
+                            <li><a href="showhistory.php">History</a></li>							
+                            <li><a href="#" id="logout">Sign out</a></li>
+                        </ul>
+                        </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="jumbotron text-center">
+        <h1>PPDJ <span class="sigma">Σ</span>-nema</h1> 
+    </div>
+    <br><br>
+    
+    <div class="container" id="homepage">
 		<h1>Opps</h1>
-		<p>Incorrect username or password, Sign in again >> <a href="customer-signin.html"></a></p>
+		<p>Incorrect username or password, Sign in again >> <a href="customer-signin.html">Sign in</a></p>
 
 	</div>
+    
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <footer class="container-fluid text-center">
+        <a href="#" title="To Top">
+            <span class="glyphicon glyphicon-chevron-up"></span>
+        </a>
+        <p>PPDJ-cinema.com</p>
+    </footer>
 </body>
 </html>
 	';
