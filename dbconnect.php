72fe4a7fa90e9389cffe9237ba58aaadf57ca885
0-justonce-0 <64194289+0-justonce-0@users.noreply.github.com>
2020-04-24 22:12:12 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -6,9 +6,7 @@ $password = "5a13415c33a740c9e0a29d0874cb47934033c4418872cd3aecfeeec3d6b872d6";
 $port = "5432";
 $dbname = "dd18of86m8s8od";
 
-$connection = "nysql:host=$servername, dbname=$dbname, port=$port";
-//$conn = pg_connect($connection);
-$conn = new PDO($connection,user=$username, password=$password);
+$conn = pg_connect("host=$servername, port=$port, user=$username, password=$password, dbname=$dbname");
 
 // if(mysqli_connect_errno()) {
 // 	echo 'Failed to connect to MySQL ' . mysqli_connect_error();
