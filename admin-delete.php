02a747cad0edeca1090427508fccd7f7f35f5774
0-justonce-0 <64194289+0-justonce-0@users.noreply.github.com>
2020-04-27 17:55:57 +0700

    Update admin-delete.php

--- admin-delete.php
+++ admin-delete.php
@@ -4,12 +4,12 @@ $username = $_GET['username'];
 include_once 'dbconnet.php';
 
 $sql = "DELETE FROM account WHERE number = '$username'";
-if(pg_query($conn, $sql)) {
+if(mysqli_query($conn, $sql)) {
 	echo "Deleted!";
 } else {
 	echo "Failed to delete an account.";	
 }
 
-pg_close($conn);
+mysqli_close($conn);
 
 ?>
