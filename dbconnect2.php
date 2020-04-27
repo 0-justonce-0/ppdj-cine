6990a8ed5af9dd4a080653f4310ce3087c19f618
0-justonce-0 <64194289+0-justonce-0@users.noreply.github.com>
2020-04-27 18:22:13 +0700

    Update dbconnect2.php

--- dbconnect2.php
+++ dbconnect2.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "theater";
+$servername = "us-cdbr-iron-east-01.cleardb.net";
+$username = "b8f83d13cf9bb0";
+$password = "b122fa66";
+$dbname = "heroku_05e0f09d37578ed";
 
 $con = mysqli_connect($servername,$username,$password,$dbname);
 
