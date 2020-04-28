e5e038b913afd36d63f62faa5e3cd58f62394d98
0-justonce-0 <64194289+0-justonce-0@users.noreply.github.com>
2020-04-28 21:16:19 +0700

    Update showtime.php

--- showtime.php
+++ showtime.php
@@ -11,7 +11,7 @@ if(isset($_SESSION['user'])=="") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
+    <title>PPDJ - Showtime</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -64,7 +64,6 @@ if(isset($_SESSION['user'])=="") {
                         <span class="caret"></span>
                     </a>
                     <ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">
-                        <li><a href="showhistory.php">History</a></li>
                         <li><a href="#" id="logout">Sign out</a></li>
                     </ul>
                 </li>
