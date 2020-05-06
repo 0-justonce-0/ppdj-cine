1546e6e75176ae5bd9ed7bd28c07c23528bd4b21
0-justonce-0 <64194289+0-justonce-0@users.noreply.github.com>
2020-05-07 03:21:05 +0700

    Update completed.php

--- completed.php
+++ completed.php
@@ -12,7 +12,7 @@ if(isset($_SESSION['user'])=="") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PPDJ - Booking history</title>
+    <title>PPDJ - Booking completed</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
